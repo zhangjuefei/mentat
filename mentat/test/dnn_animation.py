739808f83f5ceed59ba4f525a88a9111f18be4c1
zhangjuefei <zhangjuefei83@163.com>
2018-04-14 21:08:39 +0800

    add train animation

--- mentat/test/dnn_animation.py
+++ mentat/test/dnn_animation.py
@@ -4,7 +4,7 @@ from matplotlib import animation
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
 from mpl_toolkits.mplot3d import Axes3D
-from sklearn.datasets import make_circles
+from sklearn.datasets import make_circles, make_moons
 
 from mentat import ZDataFrame
 from mentat.evaluator import ClassificationEvaluator
@@ -15,6 +15,7 @@ max_epochs = 40
 hidden_neurons = 3
 
 cm = plt.cm.coolwarm
+blues = plt.cm.Blues
 cm_bright = ListedColormap(["#0000FF", "#FF0000"])
 
 # figure axes
@@ -27,7 +28,7 @@ axes.append(fig.add_subplot(2, 2, 4))
 fig.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0.2, hspace=0.2)
 
 # data points
-X, y = make_circles(n_samples=120, noise=0.24, factor=0.2, random_state=42)
+X, y = make_circles(n_samples=100, noise=0.24, factor=0.2, random_state=42)
 points = StandardScaler().fit_evaluate(
     ZDataFrame(pd.DataFrame(np.c_[X, y], columns=["x", "y", "z"]), response_column="z",
                response_encode="multiclass").impute("mean"))
@@ -42,7 +43,7 @@ train, test = points.split(0.7)
 
 # neural network
 dnn = DNN(input_shape=2, shape=[hidden_neurons, 2], activations=["sigmoid", "identity"], eta=0.2, softmax=True,
-          max_epochs=1, minibatch_size=20, verbose=True, decay_power=0.2, regularization=1e-4)
+          max_epochs=1, minibatch_size=20, verbose=False, decay_power=0.2, regularization=1e-4)
 evaluator = ClassificationEvaluator()
 
 # loss
@@ -57,20 +58,33 @@ def draw(idx):
     c_train = 1 - train()["z"]
     c_test = 1 - test()["z"]
 
-    dnn.fit(train)
     train_predict = dnn.evaluate(train)
     hidden_outputs_train = dnn.outputs[dnn.depth - 1].transpose().A
     test_predict = dnn.evaluate(test)
     hidden_outputs_test = dnn.outputs[dnn.depth - 1].transpose().A
 
+    w = (dnn.weights[dnn.depth - 1][0,:] - dnn.weights[dnn.depth - 1][1,:]).A1
+    b = (dnn.biases[dnn.depth - 1][0, :] - dnn.biases[dnn.depth - 1][1, :])[0][0]
+    xxx_min = min(np.min(hidden_outputs_train[:, 0]), np.min(hidden_outputs_test[:, 0]))
+    xxx_max = max(np.max(hidden_outputs_train[:, 0]), np.max(hidden_outputs_test[:, 0]))
+    yyy_min = min(np.min(hidden_outputs_train[:, 1]), np.min(hidden_outputs_test[:, 1]))
+    yyy_max = max(np.max(hidden_outputs_train[:, 1]), np.max(hidden_outputs_test[:, 1]))
+    xxx_range = (xxx_max - xxx_min) / 100
+    yyy_range = (yyy_max - yyy_min) / 100
+    xxx, yyy = np.meshgrid(np.arange(xxx_min, xxx_max, xxx_range), np.arange(yyy_min, yyy_max, yyy_range))
+    zzz = -w[0] / w[2] * xxx - w[1] / w[2] * yyy - b/w[2]
+
     train_accuracy.append(evaluator.fit(train_predict).metrics["accuracy"])
     test_accuracy.append(evaluator.fit(test_predict).metrics["accuracy"])
-    loss.append(dnn.epoch_loss[-1])
 
-    if idx == 0:
-        train_accuracy.append(train_accuracy[0])
-        test_accuracy.append(test_accuracy[0])
-        loss.append(loss[0])
+    if idx >= 1:
+        loss.append(dnn.epoch_loss[-1])
+    else:
+        prob = dnn.predict(train.features())
+        first_loss = np.mean(-np.sum(np.multiply(train.response(), np.log(prob + 1e-1000)), axis=0))
+        loss.append(first_loss)
+
+    print("LOSS: {:.3f}".format(loss[-1]))
 
     probability = dnn.predict(np.c_[xx.ravel(), yy.ravel()])[:, 0].reshape(xx.shape)
 
@@ -79,7 +93,7 @@ def draw(idx):
     current_loss = loss[-1] if len(loss) else 0.0
 
     axes[0].clear()
-    axes[0].set_title(r"$probability\ surface$", fontsize=8)
+    axes[0].set_title(r"$predicted\ probability\ surface$", fontsize=8)
     axes[0].plot_surface(xx, yy, probability, rstride=1, cstride=1, alpha=0.6, cmap=cm)
     axes[0].contourf(xx, yy, probability, zdir='z', offset=bottom, alpha=0.6, cmap=cm)
     axes[0].scatter(train()["x"], train()["y"], bottom, c=c_train, cmap=cm_bright, edgecolors='k',
@@ -88,10 +102,11 @@ def draw(idx):
                     alpha=0.5, s=10)
     axes[0].set_xlim(xx.min(), xx.max())
     axes[0].set_ylim(yy.min(), yy.max())
+    axes[0].set_zlim([bottom, 1.0])
     axes[0].set_xlabel(r"$x_1$", fontsize=8)
     axes[0].set_ylabel(r"$x_2$", fontsize=8)
     axes[0].tick_params(labelsize=8)
-    axes[0].view_init(40, -45)
+    axes[0].view_init(40, -30)
 
     axes[1].clear()
     axes[1].set_title(r"$epochs: {:d}/{:d}$".format(idx, max_epochs), fontsize=8)
@@ -107,18 +122,22 @@ def draw(idx):
     axes[1].grid()
 
     axes[2].clear()
-    axes[2].set_title(r"$outputs\ of\ 3\ hidden\ neurons$", fontsize=8)
+    axes[2].set_title(r"$outputs\ of\ hidden\ layer\ &\ seperating \ plane\ of\ last\ layer$", fontsize=8)
     axes[2].scatter(hidden_outputs_train[:, 0], hidden_outputs_train[:, 1], hidden_outputs_train[:, 2],
                     c=c_train,
                     cmap=cm_bright, edgecolors="k", s=10)
     axes[2].scatter(hidden_outputs_test[:, 0], hidden_outputs_test[:, 1], hidden_outputs_test[:, 2],
                     c=c_test,
                     cmap=cm_bright, edgecolors="k", alpha=0.6, s=10)
+    axes[2].plot_surface(xxx, yyy, zzz, rstride=1, cstride=1, alpha=0.5, cmap=blues, vmin=0.0, vmax=1.0)
     axes[2].set_xlabel(r"$1st\ neuron$", fontsize=8)
     axes[2].set_ylabel(r"$2nd\ neuron$", fontsize=8)
     axes[2].set_zlabel(r"$3rd\ neuron$", fontsize=8)
     axes[2].tick_params(labelsize=8)
-    axes[2].view_init(40, 45)
+    axes[2].set_xlim([0.0, 1.0])
+    axes[2].set_ylim([0.0, 1.0])
+    axes[2].set_zlim([0.0, 1.0])
+    axes[2].view_init(40, -30)
     axes[2].grid()
 
     axes[3].clear()
@@ -127,8 +146,8 @@ def draw(idx):
     axes[3].fill_between(np.arange(len(loss)), [0] * len(loss), loss, alpha=0.6)
     axes[3].plot(train_accuracy, linewidth=1)
     axes[3].plot(test_accuracy, linewidth=1)
-    axes[3].set_ylim([0, 1])
-    axes[3].set_xlim([0, len(loss)-1 if len(loss) else 0])
+    axes[3].set_ylim([0, 1.1])
+    axes[3].set_xlim([0, len(loss)-1 if len(loss) > 1 else 1])
     axes[3].legend(["loss ({:.3f})".format(current_loss), "accuracy train ({:.3f})".format(train_acc),
                     "accuracy test ({:.3f})".format(test_acc)], loc="lower left", fontsize=8)
     axes[3].set_xlabel(r"$epochs$", fontsize=8)
@@ -143,7 +162,8 @@ def init():
 
 # update
 def update(idx):
-    draw(idx)
+    dnn.fit(train)
+    draw(idx + 1)
 
 
 anim = animation.FuncAnimation(fig, update, init_func=init, frames=max_epochs, interval=220, blit=False)
